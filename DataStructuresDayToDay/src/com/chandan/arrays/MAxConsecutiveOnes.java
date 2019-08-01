a739db861cad96902c8e0e8df1eb8065c740824e
chandankumarj <chandankumarj@192.168.0.101>
2019-08-01 21:24:11 +0530

    first

---
+++ DataStructuresDayToDay/src/com/chandan/arrays/MAxConsecutiveOnes.java
@@ -0,0 +1,14 @@
+package com.chandan.arrays;
+/* One binary array is there and find the maximum of consecutive ones.
+ * 
+ * 
+ * 
+ * */
+public class MAxConsecutiveOnes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
