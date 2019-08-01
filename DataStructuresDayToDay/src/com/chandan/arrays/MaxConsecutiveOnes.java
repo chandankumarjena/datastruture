041bfaf5eb857e47a410ca8774ae6b8701c17e68
chandankumarj <chandankumarj@192.168.0.101>
2019-08-01 21:29:38 +0530

    sdsd

---
+++ DataStructuresDayToDay/src/com/chandan/arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+package com.chandan.arrays;
+/* One binary array is there and find the maximum of consecutive ones.
+ * Ex arr = [1,1,0,0,0,1,1,1,1], output = 4
+ * Note - The input array will contain only 0 and 1
+ * Time Complexity:- O(n)
+ * 
+ * */
+public class MaxConsecutiveOnes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
